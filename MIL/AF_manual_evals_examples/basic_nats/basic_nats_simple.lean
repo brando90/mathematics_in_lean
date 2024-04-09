a91f04a719cc980351106a838afae6a356dafc3e
Brando Miranda <brandojazz@gmail.com>
2024-04-09 10:49:52 -0700

    other examle

---
+++ MIL/AF_manual_evals_examples/basic_nats/basic_nats_simple.lean
@@ -0,0 +1,48 @@
+---- Example: define unary natural numbers
+
+---- define unary nats
+-- define unary natural numbers
+inductive UnaryNat : Type
+| Zero: UnaryNat
+| Succ: UnaryNat -> UnaryNat
+-- make unary nats printable
+deriving Repr
+
+-- bring contents of unary nat into scope
+open UnaryNat
+
+---- check types and evals
+-- check type of unary nat, zero and succ
+#check UnaryNat
+#check UnaryNat.Zero
+#check Succ (Succ Zero)
+
+---- define addition for unary natural numbers
+-- define addition for unary natural numbers (without explicit names in function declaration)
+def add_left : UnaryNat -> UnaryNat -> UnaryNat
+| Zero, n => n
+| Succ m, n => Succ (add_left m n)
+
+-- define addition infix notation
+infixl:65 "+" => add_left
+
+---- evals for addition
+-- eval addition for unary natural numbers left and right
+#eval Zero + Zero
+#eval Zero + (Succ Zero)
+#eval (Succ Zero) + (Succ Zero)
+
+---- theorem show non inductive case of addition
+-- theorem left addition, 0 + n = n (not inductive proof)
+theorem add_left_zero_plus_n_eq_n (n : UnaryNat) : Zero + n = n := by rfl
+-- theorem left addition, 0 + n = n (not inductive proof) with forall statements
+theorem add_left_zero_plus_n_eq_n' : ∀ n : UnaryNat, Zero + n = n := by
+  intros
+  rfl
+
+-- theorem left addition, n + 0 = n (inductive proof) with forall and use inductive hypothesis explicitly
+theorem add_left_n_plus_zero_eq_n' : ∀ (n : UnaryNat), n + Zero = n := by
+  intros n
+  induction n with
+  | Zero => rfl
+  | Succ n' ih => simp [add_left]; assumption
